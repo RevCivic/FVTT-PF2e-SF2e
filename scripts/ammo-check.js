eb14fa396bec7877efeebeabde19d54e8829fcbd
RevCivic <mjshank225@gmail.com>
2022-09-06 15:50:30 -0400

    script reference update

--- scripts/ammo-check.js
+++ scripts/ammo-check.js
@@ -1,6 +1,6 @@
 import { ConsumablePF2e } from "@item";
 
-ConsumablePF2e.prototype.isAmmoFor = function(item : ItemPF2e){
+ConsumablePF2e.isAmmoFor = function(item : ItemPF2e): boolean {
     // if (!(item instanceof WeaponPF2e || item.traits.has("powered"))) {
     //     console.warn("cannot load a consumable into a non-powered or non-weapon item")
     //     return false
