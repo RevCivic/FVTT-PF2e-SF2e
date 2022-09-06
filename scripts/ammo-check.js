9cbd9a1869c00fdb788fa1f7996dbd3c55c8f922
RevCivic <mjshank225@gmail.com>
2022-09-06 15:33:04 -0400

    Update ammo-check.js

--- scripts/ammo-check.js
+++ scripts/ammo-check.js
@@ -1,10 +1,10 @@
 import { ConsumablePF2e } from "@item";
 
 ConsumablePF2e.prototype.isAmmoFor = function(item : ItemPF2e):boolean{
-    if (!(item instanceof WeaponPF2e) || !(item.traits.has("powered"))) {
-        console.warn("cannot load a consumable into a non-powered or non-weapon item")
-        return false
-    }
+    // if (!(item instanceof WeaponPF2e || item.traits.has("powered"))) {
+    //     console.warn("cannot load a consumable into a non-powered or non-weapon item")
+    //     return false
+    // }
 
     const { max } = this.uses;
     return ((item.traits.has("repeating" || "hb_powered")) ? max > 1 : max <= 1;
