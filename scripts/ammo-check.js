597abe8d0bab5e137061bc9edd2509c2e55a6e54
RevCivic <mjshank225@gmail.com>
2022-09-06 16:05:55 -0400

    fixing brackets

--- scripts/ammo-check.js
+++ scripts/ammo-check.js
@@ -1,11 +1,15 @@
 import { ConsumablePF2e } from "@item";
 
-ConsumablePF2e.isAmmoFor = function(item : ItemPF2e): boolean {
+ConsumablePF2e.isAmmoFor = function(item : ItemPF2e): boolean{
     // if (!(item instanceof WeaponPF2e || item.traits.has("powered"))) {
     //     console.warn("cannot load a consumable into a non-powered or non-weapon item")
     //     return false
     // }
 
+    if(this.traits.has("hb_power-source") && item.traits.has("hb_powered")) {
+        return true
+    }
+
     const { max } = this.uses;
-    return ((item.traits.has("repeating" || "hb_powered")) ? max > 1 : max <= 1;
+    return ((item.traits.has("repeating")) ? max > 1 : max <= 1);
 }
