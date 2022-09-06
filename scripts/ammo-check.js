576da475a6262eefc31dcbacf50f6f1c5c90e858
RevCivic <mjshank225@gmail.com>
2022-09-06 16:09:24 -0400

    logic for ammo

--- scripts/ammo-check.js
+++ scripts/ammo-check.js
@@ -1,15 +1,10 @@
 import { ConsumablePF2e } from "@item";
 
-ConsumablePF2e.isAmmoFor = function(item : ItemPF2e): boolean{
-    // if (!(item instanceof WeaponPF2e || item.traits.has("powered"))) {
-    //     console.warn("cannot load a consumable into a non-powered or non-weapon item")
-    //     return false
-    // }
-
+ConsumablePF2e.isAmmoFor = function(item : ItemPF2e){
     if(this.traits.has("hb_power-source") && item.traits.has("hb_powered")) {
         return true
+    } else {
+        const { max } = this.uses;
+        return ((item.traits.has("repeating")) ? max > 1 : max <= 1);
     }
-
-    const { max } = this.uses;
-    return ((item.traits.has("repeating")) ? max > 1 : max <= 1);
 }
